cdcb4094e84ff07b47582df95def1271f501c58d
Blas <lautarotomasmendoza1@gmail.com>
2025-02-20 04:50:38 -0300

    el numero mas pequeño del array

---
+++ index.js
@@ -0,0 +1,20 @@
+let miVAR = [] ;
+
+miVAR[0] = 12;
+miVAR[1] = 300 ; 
+miVAR[2] = 9 ; 
+
+
+let contenedor = miVAR[0] ; 
+
+
+for(i = 0 ; i < 2 ; i ++) {
+
+    if( contenedor > miVAR[i + 1]){
+        contenedor = miVAR[i + 1] ;
+    }
+    
+    
+}
+console.log("el numero del arrai mas pequeño que hay es : " , contenedor ) ;
+
